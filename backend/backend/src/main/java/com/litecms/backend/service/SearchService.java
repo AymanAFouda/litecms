db87ace1e90882a934430eb8bc211f828f878785
Osama Fouda <osama.abouelkhair.fouda@gmail.com>
2026-03-29 22:15:37 +0300

    ElasticSearch

--- backend/backend/src/main/java/com/litecms/backend/service/SearchService.java
+++ backend/backend/src/main/java/com/litecms/backend/service/SearchService.java
@@ -6,6 +6,9 @@ import org.springframework.web.client.RestClient;
 
 import com.litecms.backend.entity.Content;
 
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
 @Service
 public class SearchService {
 
@@ -26,50 +29,26 @@ public class SearchService {
     public String indexContent(Content data) {
 
         return restClient.post()
-                .uri("/my-index/_doc/"+data.getContentId().toString()) // "exchange-rates" is the index name
+                .uri("/my-index/_doc/"+data.getContentId().toString()) 
                 .body(data)                  // RestClient converts the object to JSON
                 .retrieve()
                 .body(String.class);         // Elasticsearch returns a confirmation JSON
     }
+
+    public JsonNode search(String term) {
+
+        String json = restClient.post()
+                    .uri("/my-index/_search?q="+term) 
+                    //.body(term)                  // RestClient converts the object to JSON
+                    .retrieve()
+                    .body(String.class);         // Elasticsearch returns a confirmation JSON
+
+                    ObjectMapper mapper = new ObjectMapper();
+                    return mapper.readTree(json);
+    }
+
+
+
 }
 
 
-
- //#region
-/*
- 
-package com.litecms.backend.service;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
-
-import com.litecms.backend.entity.Content;
-
-@Service
-public class SearchService {
-    @Value("${elasticsearch.url}")
-    private String baseUrl; 
-    private final RestClient restClient;
-
-    public SearchService(RestClient.Builder builder) {
-        // Set your Elasticsearch host and add basic auth if needed
-
-        this.restClient = builder
-                .baseUrl(baseUrl)
-                .defaultHeader("Content-Type", "application/json")
-                .build();
-    }
-
-    public String indexContent(Content data) {
-
-        return restClient.post()
-                .uri("/content/_doc/"+data.getContentId().toString()) // "exchange-rates" is the index name
-                .body(data)                  // RestClient converts the object to JSON
-                .retrieve()
-                .body(String.class);         // Elasticsearch returns a confirmation JSON
-    }
-}
-
-*/
-//#endregion
